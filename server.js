86896cfdb2e5283993ad61fc6c3579d3d0e51620
boopies <tmn.sensei@gmail.com>
2020-01-08 01:02:41 +0900

    Heroku Deploy

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const MOVIEDEX = require('./movies-data-small')
+const MOVIEDEX = require('./movies-data')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -46,11 +47,19 @@ app.use(function validateBearerToken(req, res, next) {
     res.json(resultsFiltered)
 }
 
-
   app.get('/movie', handleGetMovies)
 
-const PORT = 8000
+  app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+  })
+
+  const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
 })
